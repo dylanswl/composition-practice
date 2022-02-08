dd61466f6b227a7584091f34adaf0bb6610b64de
Dylan Sewell <dylanswl@icloud.com>
2022-02-07 19:24:46 -0800

    first commit

---
+++ Composition/Composition.h
@@ -0,0 +1,18 @@
+//
+//  Composition.h
+//  Composition
+//
+//  Created by Dylan Sewell on 2/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Composition.
+FOUNDATION_EXPORT double CompositionVersionNumber;
+
+//! Project version string for Composition.
+FOUNDATION_EXPORT const unsigned char CompositionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Composition/PublicHeader.h>
+
+
